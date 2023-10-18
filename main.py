f66df7727b0e69601d2d040f2c71496b4c0dbfff
Tessa Thompson <tthompson996@lsr7.net>
2023-10-18 09:46:04 -0500

    import background for game

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 
 import pygame 
 
+#blank canvas
 (width, height) = (800, 600)
 screen = pygame.display.set_mode((width, height))
 pygame.display.flip()
 
-#if x is clicked
+#if x (exit) is clicked
 running = True
 while running:
   for event in pygame.event.get():
