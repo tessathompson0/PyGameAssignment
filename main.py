5cced41aaa100db8f6aeef22defbc250014168d6
Tessa Thompson <tthompson996@lsr7.net>
2023-10-30 08:31:02 -0500

    finish enemy and player class

--- main.py
+++ main.py
@@ -2,20 +2,4 @@
 #10.18.23
 #King Curtis Game
 
-import pygame 
-import time
-pygame.init()
-
-from MainGame import MainGame
-
-#blank canvas
-#(width, height) = (800, 600)
-#screen = pygame.display.set_mode((width, height))
-#add name to top of screen
-pygame.display.set_caption('King Curtis Game')
-pygame.display.flip()
-
-x = MainGame()
-x.loadBackground()
-
-
+import pygame
--- player.py
+++ player.py
@@ -1,3 +1,24 @@
 import pygame
-from background import background
-background = background()
+class player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.center = (400,300)
+        self.speed = 5
+    def update(self):
+        keys = pygame.key.get_pressed()
+        #go up
+        if keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        #go down
+        if keys[pygame.K_s]:
+            self.rect.y += self.speed
+        #go left
+        if keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        #go right
+        if keys[pygame.K_d]:
+            self.rect.x +=self.speed
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
