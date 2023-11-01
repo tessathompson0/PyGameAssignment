9fa295d742b9c29d966e658189881acba8e53265
Tessa Thompson <tthompson996@lsr7.net>
2023-11-01 09:49:49 -0500

    added welcome message and win/lose message

--- enemy.py
+++ enemy.py
@@ -1,11 +1,14 @@
 import pygame
 import random
+
+#mom from wife swap sprite
 class Enemy(pygame.sprite.Sprite):
     #load sprite
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("Enemy.png").convert_alpha()
         self.rect = self.image.get_rect()
+        #keep sprite in screen
         self.rect.x = random.randrange(800 - self.rect.width)
         self.rect.y = random.randrange(600 - self.rect.height)
         self.speed = 2
@@ -13,14 +16,17 @@ class Enemy(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += random.choice([-1, 0, 1]) * self.speed
         self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
         self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
         self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#chicken nugget sprite
 class goodEnemy(pygame.sprite.Sprite):
     def __init__(self):
         #load sprite
         super().__init__()
         self.image = pygame.image.load("ChickenNugget.png").convert_alpha()
         self.rect = self.image.get_rect()
+        #keep sprite in screen
         self.rect.x = random.randrange(800 - self.rect.width)
         self.rect.y = random.randrange(600 - self.rect.height)
         self.speed = 2
@@ -28,6 +34,132 @@ class goodEnemy(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += random.choice([-1, 0, 1]) * self.speed
         self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
         self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
         self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
-
+#Bacon Sprite
+class goodEnemy2(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Bacon.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#Soda Sprite
+class goodEnemy3(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Soda.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#twinkie sprite
+class goodEnemy4(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Twinkie.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#Fruit sprite
+class Enemy2(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Fruit.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#Lettuce sprite
+class Enemy3(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Lettuce.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#tomato sprite
+class Enemy4(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Tomato.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
+#water sprite
+class Enemy5(pygame.sprite.Sprite):
+    def __init__(self):
+        #load sprite
+        super().__init__()
+        self.image = pygame.image.load("Water.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        #keep sprite in screen
+        self.rect.x = random.randrange(800 - self.rect.width)
+        self.rect.y = random.randrange(600 - self.rect.height)
+        self.speed = 2
+    #make sprite move randomly
+    def update(self):
+        self.rect.x += random.choice([-1, 0, 1]) * self.speed
+        self.rect.y += random.choice([-1, 0, 1]) * self.speed
+        #keep sprite in screen
+        self.rect.x = max(0, min(800 - self.rect.width, self.rect.x))
+        self.rect.y = max(0, min(600 - self.rect.height, self.rect.y))
--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 #10.18.23
 #King Curtis Game
 
+#import stuff
 import pygame
 from player import Player
 from enemy import Enemy
 from enemy import goodEnemy
+from enemy import Enemy2
+from enemy import Enemy3
+from enemy import Enemy4
+from enemy import Enemy5
+from enemy import goodEnemy2
+from enemy import goodEnemy3
+from enemy import goodEnemy4
 
 pygame.init()
 
@@ -21,26 +29,71 @@ pygame.display.set_caption("King Curtis Game")
 #make sprite groups
 all_sprites = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
+enemies2 = pygame.sprite.Group()
+enemies3 = pygame.sprite.Group()
+enemies4 = pygame.sprite.Group()
+enemies5 = pygame.sprite.Group()
 goodEnemies = pygame.sprite.Group()
+goodEnemies2 = pygame.sprite.Group()
+goodEnemies3 = pygame.sprite.Group()
+goodEnemies4 = pygame.sprite.Group()
 
 player = Player()
 all_sprites.add(player)
 
-#add multiple enemies
-for i in range(5):
+#spawn sprites
+for i in range(2):
     enemy = Enemy()
     all_sprites.add(enemy)
     enemies.add(enemy)
-for i in range(5):
+for i in range(2):
     goodenemy = goodEnemy()
     all_sprites.add(goodenemy)
     goodEnemies.add(goodenemy)
+for i in range(2):
+    enemy2 = Enemy2()
+    all_sprites.add(enemy2)
+    enemies2.add(enemy2)
+for i in range(2):
+    enemy3 = Enemy3()
+    all_sprites.add(enemy3)
+    enemies3.add(enemy3)
+for i in range(2):
+    enemy4 = Enemy4()
+    all_sprites.add(enemy4)
+    enemies4.add(enemy4)
+for i in range(2):
+    enemy5 = Enemy5()
+    all_sprites.add(enemy5)
+    enemies5.add(enemy5)
+for i in range(2):
+    goodenemy2 = goodEnemy2()
+    all_sprites.add(goodenemy2)
+    goodEnemies2.add(goodenemy2)
+for i in range(2):
+    goodenemy3 = goodEnemy3()
+    all_sprites.add(goodenemy3)
+    goodEnemies2.add(goodenemy3)
+for i in range(2):
+    goodenemy4 = goodEnemy4()
+    all_sprites.add(goodenemy4)
+    goodEnemies4.add(goodenemy4)
 
 #start a score system
 score = 0
 
 #make background a png image
 background = pygame.image.load("Background.png").convert()
+#make background if you win
+youWin = pygame.image.load("YouWin.png").convert()
+#make background if you lose
+youLose = pygame.image.load("YouLose.png").convert()
+#make welcome message load
+welcomeMessage = pygame.image.load("WelcomeMessage.png").convert()
+
+#game start/end variables
+game_started = False
+game_over = False
 
 #keep running until x button is pushed
 running = True
@@ -49,29 +102,125 @@ while running:
         #if person presses x
         if event.type == pygame.QUIT:
             running = False
-    #make background appear
-    screen.blit(background, (0,0))
-    all_sprites.update()
-    hits = pygame.sprite.spritecollide(player, enemies, True)
-    #gain and lose points with enemies
-    for enemy in hits:
-        score -= 1
-        new_enemy = Enemy()
-        all_sprites.add(new_enemy)
-        enemies.add(new_enemy)
-    hits2 = pygame.sprite.spritecollide(player, goodEnemies, True)
-    for goodenemy in hits2:
-        score += 1
-        new_goodEnemy = goodEnemy()
-        all_sprites.add(new_goodEnemy)
-        enemies.add(new_goodEnemy)
-    #make sprites show up on screen
-    all_sprites.draw(screen)
-    #show points scored
-    font = pygame.font.Font(None, 36)
-    score_text = font.render("Score: {}".format(score),True,(255, 0, 0))
-    screen.blit(score_text, (10, 10))
+        #starts game if background is pressed
+        elif event.type == pygame.MOUSEBUTTONDOWN and not game_started:
+            game_started = True
+        #welcome message stays until button is clicked
+    if not game_started:
+        screen.blit(welcomeMessage, (0,0))
+    else:
+        #make background appear
+        screen.blit(background, (0,0))
+        all_sprites.update()
+        #collision between player and wife swap mom (-10 points)
+        hits = pygame.sprite.spritecollide(player, enemies, True)
+        #collision between player and chicken nugget (+10 points)
+        hits2 = pygame.sprite.spritecollide(player, goodEnemies, True)
+        #collision between player and fruit (-4 points)
+        hits3 = pygame.sprite.spritecollide(player, enemies2, True)
+        #collision between player and Lettuce (-8 points)
+        hits4 = pygame.sprite.spritecollide(player, enemies3, True)
+        #collision between player and Tomato (-2 points)
+        hits5 = pygame.sprite.spritecollide(player, enemies4, True)
+        #collision between player and water (-1 point)
+        hits6 = pygame.sprite.spritecollide(player, enemies5, True)
+        #collision between player and bacon (+8 points)
+        hits7 = pygame.sprite.spritecollide(player, goodEnemies2, True)
+        #collision between player and soda (+2 points)
+        hits8 = pygame.sprite.spritecollide(player, goodEnemies3, True)
+        #collision between player and twinkies (+4 points)
+        hits9 = pygame.sprite.spritecollide(player, goodEnemies4, True)
+        #collision 1 (with wife swap mom)
+        for enemy in hits:
+            #change score
+            score = score - 10
+            #add new enemy to replace old enemy
+            new_enemy = Enemy()
+            all_sprites.add(new_enemy)
+            enemies.add(new_enemy)
+        #collision 2 (with chicken nugget)
+        for goodenemy in hits2:
+            #change
+            score = score + 10
+            new_goodEnemy = goodEnemy()
+            all_sprites.add(new_goodEnemy)
+            goodEnemies.add(new_goodEnemy)
+        #collision 3 (with fruit)
+        for enemy2 in hits3:
+            #change score
+            score = score - 4
+            #add new enemy to replace old enemy
+            new_enemy2 = Enemy2()
+            all_sprites.add(new_enemy2)
+            enemies2.add(new_enemy2)
+        #collision 4 (with Lettuce)
+        for enemy3 in hits4:
+            #change score
+            score = score - 8
+            #add new enemy to replace old enemy
+            new_enemy3 = Enemy3()
+            all_sprites.add(new_enemy3)
+            enemies3.add(new_enemy3)
+        #collision 5 (with Tomato)
+        for enemy4 in hits5:
+            #change score
+            score = score - 2
+            #add new enemy to replace old enemy
+            new_enemy4 = Enemy4()
+            all_sprites.add(new_enemy4)
+            enemies4.add(new_enemy4)
+        #collision 6 (with Water)
+        for enemy5 in hits6:
+            #change score
+            score = score - 2
+            #add new enemy to replace old enemy
+            new_enemy5 = Enemy5()
+            all_sprites.add(new_enemy5)
+            enemies5.add(new_enemy5)
+        #collision 7 (with bacon)
+        for goodenemy2 in hits7:
+            #change score
+            score = score + 8
+            #add new enemy to replace old enemy
+            new_goodEnemy2 = goodEnemy2()
+            all_sprites.add(new_goodEnemy2)
+            goodEnemies2.add(new_goodEnemy2)
+        #collision 8 (with soda)
+        for goodenemy3 in hits8:
+            #change score
+            score = score + 2
+            #add new enemy to replace old enemy
+            new_goodEnemy3 = goodEnemy3()
+            all_sprites.add(new_goodEnemy3)
+            goodEnemies3.add(new_goodEnemy3)
+        #collision 8 (with Twinkies)
+        for goodenemy4 in hits9:
+        #change score
+            score = score + 4
+            #add new enemy to replace old enemy
+            new_goodEnemy4 = goodEnemy4()
+            all_sprites.add(new_goodEnemy4)
+            goodEnemies4.add(new_goodEnemy4)
+
+        #make sprites show up on screen
+        all_sprites.draw(screen)
+        #end game if you get 100 or more points (you win)
+        if score >= 100:
+            game_over = True
+            screen.blit(youWin, (0,0))
+        #end game if you get -50 points or less (you lose)
+        if score <= -50:
+            game_over = True
+            screen.blit(youLose, (0,0))
+        #show points scored
+        font = pygame.font.Font(None, 36)
+        score_text = font.render("Score: {}".format(score),True,(255, 0, 0))
+        screen.blit(score_text, (10, 10))
 
     pygame.display.flip()
+    #exit when game is over (wait 5 seconds to show message)
+    if game_over:
+        pygame.time.delay(5000)
+        running = False
 
 pygame.quit()
